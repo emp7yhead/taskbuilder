144c82cc5195607dcc550d55c42c48044cc512f5
emp7yhead <artyomkropp@gmail.com>
2023-07-09 01:56:42 +0900

    refactor get_task route

--- app/api/v1/task.py
+++ app/api/v1/task.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, Depends
 
 from app.schemas.build import BuildIn
 from app.services.builds import get_builds_tree, get_current_tasks
-from app.services.tasks import build_tasks_graph, get_tasks_tree
+from app.services.tasks import build_tasks_graph
 from app.utils.tree import get_dependencies
 
 task_router = APIRouter()
@@ -19,8 +19,9 @@ task_router = APIRouter()
 async def get_tasks(
     build_name: BuildIn,
     builds_tree=Depends(get_builds_tree),
-    tasks_tree=Depends(get_tasks_tree),
+    tasks_graph=Depends(build_tasks_graph),
 ):
-    current_tasks: list[str] = get_current_tasks(builds_tree, build_name)
-    tasks_graph: dict[str, str] = build_tasks_graph(tasks_tree)
+    current_tasks: list[str] = get_current_tasks(
+        build_name.build, builds_tree
+    )
     return get_dependencies(tasks_graph, current_tasks)
--- app/services/tasks.py
+++ app/services/tasks.py
@@ -1,3 +1,7 @@
+import logging
+
+from fastapi import Depends, HTTPException
+
 from app.settings import settings
 from app.utils.reader import build_path, get_data
 
@@ -5,10 +9,13 @@ from app.utils.reader import build_path, get_data
 def get_tasks_tree() -> dict:
     tasks_path = build_path(settings.tasks_file)
     tasks_data = get_data(tasks_path)
+    if not tasks_data:
+        logging.info('Tasks file empty')
+        raise HTTPException(500, 'Tasks are empty')
     return tasks_data['tasks']
 
 
-def build_tasks_graph(tasks_tree):
+def build_tasks_graph(tasks_tree=Depends(get_tasks_tree)):
     return {
         task['name']: task['dependencies']
         for task

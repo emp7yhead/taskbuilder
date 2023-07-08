a7218fbf39c84bfc1a9ba5e1373ef6ae2e42b03e
emp7yhead <artyomkropp@gmail.com>
2023-07-09 01:59:39 +0900

    add builds and healthcheck routes

--- app/api/base.py
+++ app/api/base.py
@@ -1,6 +1,11 @@
-from fastapi import APIRouter
+from http import HTTPStatus
 
+from fastapi import APIRouter, Depends
+
+from app.api.v1.build import build_router
 from app.api.v1.task import task_router
+from app.services.builds import get_builds_tree
+from app.services.tasks import get_tasks_tree
 
 main_router = APIRouter()
 
@@ -9,8 +14,20 @@ main_router.include_router(
     prefix='/tasks',
     tags=['tasks'],
 )
+main_router.include_router(
+    build_router,
+    prefix='/builds',
+    tags=['builds'],
+)
 
 
-@main_router.get('/ping')
-async def ping():
-    return 'pong'
+@main_router.get(
+    '/healthcheck',
+    response_model=str,
+    status_code=HTTPStatus.OK,
+    description='Check existence of needed files for service.',)
+async def check_service(
+    builds: dict = Depends(get_builds_tree),
+    tasks: dict = Depends(get_tasks_tree),
+):
+    return 'All services available'

548be771f7216cf5e4c2a32c9aba1a1e1526c917
emp7yhead <artyomkropp@gmail.com>
2023-07-09 01:57:01 +0900

    update treewalking algorithm

--- app/utils/tree.py
+++ app/utils/tree.py
@@ -1,33 +1,26 @@
 import logging
 
-from fastapi import HTTPException
-
 from app.services.tasks import get_unique
 
 
-def get_dependencies(graph: dict, tasks: list[str]):
+def get_dependencies(graph: dict, tasks: list[str]) -> list:
     """
     Dependencies not in dict are 'white'
     Not finished dependencies marked as 'grey'
     Added dependencies is 'black'
     """
-    color = dict()
+    visited = set()
     order = []
 
     def dfs_walk(node):
         logging.info(f'Get dependencies for "{node}" task')
-        color[node] = 'grey'
+        visited.add(node)
         for successor in graph[node]:
-            if color.get(successor) == 'grey':
-                raise HTTPException(
-                    422, f'Circular dependencies found in {successor}'
-                )
-            if successor not in color:
+            if successor not in visited:
                 dfs_walk(successor)
             else:
                 logging.info(f'"{node}" already in dependecies')
         order.append(node)
-        color[node] = 'black'
 
     for task in get_unique(tasks):
         dfs_walk(task)
